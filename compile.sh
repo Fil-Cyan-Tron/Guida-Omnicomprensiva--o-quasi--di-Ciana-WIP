e7a7643ee99d5e75dff0353d1ef4817b1a336959
Fil-Cyan-Tron <filtron11@gmail.com>
2023-05-24 14:54:06 +0200

    E chi si lamenta?

--- compile.sh
+++ compile.sh
@@ -6,3 +6,4 @@ cd Compendio-Semi-Omnicomprensivo-di-Ciana/
 git commit -a -m "Però, quanto a te, quanto a quello che non puoi committare che tu, qualcosa che tu possa pushare ci dovrebbe essere, ma non ti costringerà nessuno. Pensa da te stesso, decidi da te stesso, quello che adesso tu stesso debba syncare."
 git push origin main
 cd ..
+pdflatex Guida.tex
