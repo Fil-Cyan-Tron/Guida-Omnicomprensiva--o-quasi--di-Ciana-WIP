64fa754694d79b7470453d1f0ea6a65a1d6f70e5
Fil-Cyan-Tron <filtron11@gmail.com>
2023-05-24 14:55:45 +0200

    E chi si lamenta?

--- compile.sh
+++ compile.sh
@@ -6,4 +6,5 @@ cd Compendio-Semi-Omnicomprensivo-di-Ciana/
 git commit -a -m "Però, quanto a te, quanto a quello che non puoi committare che tu, qualcosa che tu possa pushare ci dovrebbe essere, ma non ti costringerà nessuno. Pensa da te stesso, decidi da te stesso, quello che adesso tu stesso debba syncare."
 git push origin main
 cd ..
-pdflatex Guida.tex
+git commit -a -m "E chi si lamenta?"
+git push origin main
