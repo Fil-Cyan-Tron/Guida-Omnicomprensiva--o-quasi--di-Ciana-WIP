c81b1c474df605e080d0621fc3ef0e65140925ce
Fil-Cyan-Tron <filtron11@gmail.com>
2023-09-26 11:24:15 +0200

    Però, quanto a te, quanto a quello che non puoi committare che tu, qualcosa che tu possa pushare ci dovrebbe essere, ma non ti costringerà nessuno. Pensa da te stesso, decidi da te stesso, quello che adesso tu stesso debba syncare.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 # BIN/BASH
 pdflatex Guida.tex
 git commit -a -m "Però, quanto a te, quanto a quello che non puoi committare che tu, qualcosa che tu possa pushare ci dovrebbe essere, ma non ti costringerà nessuno. Pensa da te stesso, decidi da te stesso, quello che adesso tu stesso debba syncare."
-git push origin master
+git push -u origin master
 cmatrix -r -s
--- release.sh
+++
@@ -1,9 +0,0 @@
-#BIN/BASH
-pdflatex Guida.tex
-cp Guida.pdf ../GuideReleases
-cd ../GuideReleases
-git remote add origin https://github.com/Fil-Cyan-Tron/GuideReleases.git
-git remote -v
-git push origin main
-cd ..
-cowsay "Guida compilata e rilasciata"
