e436901e6db1367f9ed2926e457e28886b10eda5
Fil-Cyan-Tron <filtron11@gmail.com>
2023-09-26 10:48:52 +0200

    Però, quanto a te, quanto a quello che non puoi committare che tu, qualcosa che tu possa pushare ci dovrebbe essere, ma non ti costringerà nessuno. Pensa da te stesso, decidi da te stesso, quello che adesso tu stesso debba syncare.

--- compile.sh
+++ compile.sh
@@ -4,6 +4,6 @@ cp Guida.pdf Compendio-Semi-Omnicomprensivo-di-Ciana/Guida.pdf
 date > Compendio-Semi-Omnicomprensivo-di-Ciana/date.txt
 cd Compendio-Semi-Omnicomprensivo-di-Ciana/
 git commit -a -m "Però, quanto a te, quanto a quello che non puoi committare che tu, qualcosa che tu possa pushare ci dovrebbe essere, ma non ti costringerà nessuno. Pensa da te stesso, decidi da te stesso, quello che adesso tu stesso debba syncare."
-git push origin master
+git push origin main
 cd ..
 cmatrix -r -s
