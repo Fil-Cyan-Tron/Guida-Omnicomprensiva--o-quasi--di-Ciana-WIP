318879be785af258b7d94c8f68b2d0660fa1d154
Fil-Cyan-Tron <filtron11@gmail.com>
2023-12-13 00:37:10 +0100

    Update pdfTeX version and image file usage

--- compile.sh
+++ compile.sh
@@ -1,5 +1,7 @@
 # BIN/BASH
 pdflatex Guida.tex
-git commit -a -m "Però, quanto a te, quanto a quello che non puoi committare che tu, qualcosa che tu possa pushare ci dovrebbe essere, ma non ti costringerà nessuno. Pensa da te stesso, decidi da te stesso, quello che adesso tu stesso debba syncare."
-git push -u origin master
-cmatrix -r -s
+pdflatex Guida.tex
+pdflatex Guida.tex
+pdflatex Misteri.tex
+pdflatex Misteri.tex
+pdflatex Misteri.tex
